df4ef60523f623ba410df63e33e22375a9fe9a6a
sanju-27 <sanjaystz98@gmail.com>
2019-06-13 18:37:47 +0530

    Update Ticket.java

--- src/railwayreservation/Ticket.java
+++ src/railwayreservation/Ticket.java
@@ -11,18 +11,47 @@ package railwayreservation;
  */
 public class Ticket {
     
-    
+    int tID;
     String type;
     double price;
     int count;
     double cancelPrice;
     
-    public Ticket(String type, double price, int count, double cancelPrice)
+    public Ticket(int tID, String type, double price, int count, double cancelPrice)
     {
+        this.tID = tID;
         this.type = type;
         this.cancelPrice = cancelPrice;
         this.count = count;
         this.price = price;
+    }
+
+//    public Ticket(String type) {
+//        this.type = type;
+//    }
+
+    public Ticket(int tID) {
+        this.tID = tID;
+    }
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj == this) return true;
+        Ticket t = (Ticket)obj;
+        return this.tID == t.tID;
+
+//To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 17*hash + (this.tID != 0 ? this.tID : 0);
+        return hash;
+        
+//To change body of generated methods, choose Tools | Templates.
     }
     
     public boolean book(int n)
