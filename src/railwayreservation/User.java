7afdd5d6d2ab14e75547bd4db5105f499e91f6b5
sanju-27 <sanjaystz98@gmail.com>
2019-06-13 18:37:54 +0530

    Update User.java

--- src/railwayreservation/User.java
+++ src/railwayreservation/User.java
@@ -24,9 +24,57 @@ public class User {
         this.age = age;
         bookedPnr = new ArrayList<>();
     }
-    public boolean bookTicket(Train tr, int count, )
+    public boolean bookTicket(Train tr, int count, int tID, boolean tat)
     {
-        
+        Ticket temp = new Ticket(tID);
+        if(tat==false)
+        {
+            if(tr.available.contains(temp))
+            {
+                Ticket x = tr.available.get(tr.available.indexOf(temp));
+                if(x.book(count))
+                {
+                    Pnr p = new Pnr(tr, this, x, count);
+                    p.status.concat("CNF");
+                    bookedPnr.add(p);
+                    tr.available.set(tr.available.indexOf(temp), x);
+                }
+                else
+                {
+                    Pnr p = new Pnr(tr, this, x, count);
+                    p.status.concat("WL");
+                    bookedPnr.add(p);
+    //                tr.available.set(tr.available.indexOf(temp), x);
+                    tr.waitList.add(p);
+                }
+                return true;
+            }
+            else return false;
+        }
+        else
+        {
+            if(tr.tatkal.contains(temp))
+            {
+                Ticket x = tr.tatkal.get(tr.tatkal.indexOf(temp));
+                if(x.book(count))
+                {
+                    Pnr p = new Pnr(tr, this, x, count);
+                    p.status.concat("TKL");
+                    bookedPnr.add(p);
+                    tr.tatkal.set(tr.tatkal.indexOf(temp), x);
+                }
+                else
+                {
+                    Pnr p = new Pnr(tr, this, x, count);
+                    p.status.concat("WL");
+                    bookedPnr.add(p);
+    //                tr.tatkal.set(tr.tatkal.indexOf(temp), x);
+                    tr.tWaitList.add(p);
+                }
+                return true;
+            }
+            else return false;
+        }
     }
     
     
