fa351504072303890add798aa51e6b17c023df75
sanju-27 <45707609+sanju-27@users.noreply.github.com>
2019-06-16 14:07:44 +0530

    Update RailwayReservation.java

--- src/railwayreservation/RailwayReservation.java
+++ src/railwayreservation/RailwayReservation.java
@@ -4,11 +4,8 @@
  * and open the template in the editor.
  */
 package railwayreservation;
-
 import java.util.*;
 import java.io.*;
-
-
 /**
  *
  * @author Administrator
@@ -18,24 +15,22 @@ public class RailwayReservation {
     /**
      * @param args the command line arguments
      */
-    static void writeFile(String fname, String str) {
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(fname, true));
-            bw.write(str);
-        } catch (IOException ex) {
-            System.err.println("Caught exception" + ex);
-        }
+    // static void writeFile(String fname, String str) {
+    //     try {
+    //         BufferedWriter bw = new BufferedWriter(new FileWriter(fname, true));
+    //         bw.write(str);
+    //     } catch (IOException ex) {
+    //         System.err.println("Caught exception" + ex);
+    //     }
 
-    }
+    // }
 
     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Admin admin = (Admin)Admin.readFile();
-        if(admin==null)
+        Admin admin = (Admin) Admin.readFile();
+        if (admin == null)
             admin = new Admin();
-        String userFile = "C:/Users/Administrator/Desktop/userFile";
+        // String userFile = "C:/Users/Administrator/Desktop/userFile";
         int id = 3;
         do {
             System.out.print("Welcome to Railway Reservation\nEnter ID / Enter -1 to exit: ");
@@ -48,148 +43,146 @@ public class RailwayReservation {
                     if (ch1 == 1) {
                         int ch2;
                         System.out.println("1.Add\n2.View\n3.Count\n4.Exit");
-                        do
-                        {
+                        do {
                             ch2 = Integer.parseInt(br.readLine());
-                            switch (ch2)
-                            {
-                                case 1:
-                                    System.out.print("Enter Name: ");
-                                    String name = br.readLine();
-                                    System.out.print("Enter Phone number: ");
-                                    String ph = br.readLine();
-                                    System.out.print("Entetr age: ");
-                                    int age = Integer.parseInt(br.readLine());
-                                    admin.users.add(new User(admin.users.size(), name, ph, age));
-                                    break;
-                                case 2:
-                                    System.out.print("Enter UID: ");
-                                    int uid = Integer.parseInt(br.readLine());
-                                    System.out.println(admin.users.get(uid).toString());
-                                    break;  
-                                case 3:
-                                    System.out.println("Total number of users: "+admin.users.size());
+                            switch (ch2) {
+                            case 1:
+                                System.out.print("Enter Name: ");
+                                String name = br.readLine();
+                                System.out.print("Enter Phone number: ");
+                                String ph = br.readLine();
+                                System.out.print("Entetr age: ");
+                                int age = Integer.parseInt(br.readLine());
+                                admin.users.add(new User(admin.users.size(), name, ph, age));
+                                break;
+                            case 2:
+                                System.out.print("Enter UID: ");
+                                int uid = Integer.parseInt(br.readLine());
+                                System.out.println(admin.users.get(uid).toString());
+                                break;
+                            case 3:
+                                System.out.println("Total number of users: " + admin.users.size());
                             }
-                        }while(ch2<3);
+                        } while (ch2 < 3);
                     }
-                    if(ch1==2)
-                    {
+                    if (ch1 == 2) {
                         int ch2;
                         System.out.println("1.Add\n2.View\n3.Count\n4.Exit");
-                        do
-                        {
+                        do {
                             ch2 = Integer.parseInt(br.readLine());
-                            switch (ch2)
-                            {
-                                case 1:
-                                    System.out.print("Enter Train number: ");
-                                    int num = Integer.parseInt(br.readLine());
-                                    Train t = new Train(num);
-                                    List<Ticket> gen = new ArrayList<>();
-                                    List<Ticket> tat = new ArrayList<>();
-                                    System.out.print("Enter Type count: ");
-                                    int n = Integer.parseInt(br.readLine());
-                                    System.out.println("TID\tType\tGeneral\tTatkal\tPrice\tCancel Fee");
-                                    for(int i = 0; i<n; i++)
-                                    {
-                                        String[] s = br.readLine().split(" ");
-                                        Ticket gt = new Ticket(Integer.parseInt(s[0]),s[1],Double.valueOf(s[4]),Integer.parseInt(s[2]),Double.valueOf(s[5]));
-                                        Ticket tt = new Ticket(Integer.parseInt(s[0]),s[3],Double.valueOf(s[4]),Integer.parseInt(s[2]),Double.valueOf(s[5]));
-                                        gen.add(gt);
-                                        tat.add(tt);
-                                    }
-                                    Map<String,String> mp = new HashMap<>();
-                                    System.out.println("Enter Route Details\n");
-                                    System.out.print("Number of Stops: ");
-                                    n = Integer.parseInt(br.readLine());
-                                    System.out.println("Place\tTime");
-                                    for (int i = 0; i < n; i++) {
-                                        String s[] = br.readLine().split(" ");
-                                        mp.put(s[0], s[1]);
-                                    }
-                                    t.setRoute(mp);
-                                    t.setAvailable(gen);
-                                    t.setTatkal(tat);
-                                    admin.trains.add(t);
-                                    break;
-                                case 2:
-                                    System.out.print("Enter Train number: ");
-                                    int uid = Integer.parseInt(br.readLine());
-                                    System.out.println(admin.users.get(uid).toString());
-                                    break;  
-                                case 3:
-                                    System.out.println("Total number of users: "+admin.users.size());
+                            switch (ch2) {
+                            case 1:
+                                System.out.print("Enter Train number: ");
+                                int num = Integer.parseInt(br.readLine());
+                                Train t = new Train(num);
+                                List<Ticket> gen = new ArrayList<>();
+                                List<Ticket> tat = new ArrayList<>();
+                                System.out.print("Enter Type count: ");
+                                int n = Integer.parseInt(br.readLine());
+                                System.out.println("TID\tType\tGeneral\tTatkal\tPrice\tCancel Fee");
+                                for (int i = 0; i < n; i++) {
+                                    String[] s = br.readLine().split(" ");
+                                    Ticket gt = new Ticket(Integer.parseInt(s[0]), s[1], Double.valueOf(s[4]),
+                                            Integer.parseInt(s[2]), Double.valueOf(s[5]));
+                                    Ticket tt = new Ticket(Integer.parseInt(s[0]), s[3], Double.valueOf(s[4]),
+                                            Integer.parseInt(s[2]), Double.valueOf(s[5]));
+                                    gen.add(gt);
+                                    tat.add(tt);
+                                }
+                                Map<String, String> mp = new HashMap<>();
+                                System.out.println("Enter Route Details\n");
+                                System.out.print("Number of Stops: ");
+                                n = Integer.parseInt(br.readLine());
+                                System.out.println("Place\tTime");
+                                for (int i = 0; i < n; i++) {
+                                    String s[] = br.readLine().split(" ");
+                                    mp.put(s[0], s[1]);
+                                }
+                                t.setRoute(mp);
+                                t.setAvailable(gen);
+                                t.setTatkal(tat);
+                                admin.trains.add(t);
+                                break;
+                            case 2:
+                                System.out.print("Enter Train number: ");
+                                int uid = Integer.parseInt(br.readLine());
+                                System.out.println(admin.users.get(uid).toString());
+                                break;
+                            case 3:
+                                System.out.println("Total number of users: " + admin.users.size());
                             }
-                        }while(ch2<3);
+                        } while (ch2 < 3);
                     }
                 } while (ch1 < 3);
-            }
-            else
-            {
+            } else {
                 User u = admin.users.get(id);
-                System.out.println("Welcome back "+u.name);
+                System.out.println("Welcome back " + u.name);
                 int ch1;
-                do
-                {
-                    System.out.println("1.Book Train\n2.View Booked PNR\n3.View My Details\n4. Exit");
+                do {
+                    System.out.println("1.Book Train\n2.View Booked PNR\n3.Cancel Tickets\n4.View My Details\n5. Exit");
                     ch1 = Integer.parseInt(br.readLine());
-                    switch(ch1)
-                    {
-                        case 1:
-                            Pnr p = bookTicket(admin,u);
-                            viewPNR(p);
-                            break;
-                        case 2:
-                            viewPNR(u);
-                            break;
-                        case 3:
-                            System.out.println(u.toString());
-                            break;
+                    switch (ch1) {
+                    case 1:
+                        Pnr p = bookTicket(admin, id);
+                        viewPNR(p);
+                        break;
+                    case 2:
+                        viewPNR(u);
+                        break;
+                    case 3:
+                        viewPNR(u);
+                        System.out.print("Select Ticket to cancel: ");
+                        int n = Integer.parseInt(br.readLine());
+                        Pnr pnr = u.bookedPnr.get(n-1);
+                        pnr.cancel();
+                        break;
+                        
+                    case 4:
+                        System.out.println(u.toString());
+                        break;
                     }
-                }while(ch1<4);
+                } while (ch1 < 5);
             }
         } while (id < 0);
+        admin.writeFile();
 
     }
 
-    private static Pnr bookTicket(Admin admin, User u) throws IOException {
-        
+    private static Pnr bookTicket(Admin admin, int uid) throws IOException {
+        User u = admin.users.get(uid);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Tatkal? [Y/N]");
-        boolean tat = br.readLine().equalsIgnoreCase("Y")?true:false;
-
+        boolean tat = br.readLine().equalsIgnoreCase("Y") ? true : false;
+        String from, to;
         List<Train> ls = new ArrayList<>();
-        dest:{
-        System.out.println("Enter the from place");
-        String from = br.readLine();
-        System.out.println("Enter to place");        
-        String to = br.readLine();
-        if(from.equals(to))
-        {
-            System.out.println("Error! From cannot be same as To\nTry again...");
-            break dest;
-        }
+        dest: {
+            System.out.println("Enter the from place");
+            from = br.readLine();
+            System.out.println("Enter to place");
+            to = br.readLine();
+            if (from.equals(to)) {
+                System.out.println("Error! From cannot be same as To\nTry again...");
+                break dest;
+            }
             for (Train t : admin.trains) {
-                if(t.route.containsKey(from))
-                {
-                    if(t.route.containsKey(to))
+                if (t.route.containsKey(from)) {
+                    if (t.route.containsKey(to))
                         ls.add(t);
                 }
             }
-            }
+        }
         System.out.println("Available Trains:");
-        for (Train t: ls) {
+        for (Train t : ls) {
             System.out.println(t.trainNo);
         }
         System.out.println("Choose train number");
         int tno = Integer.parseInt(br.readLine());
         Train t = new Train(tno);
-        t = admin.trains.get(admin.trains.index(t));
+        t = admin.trains.get(admin.trains.indexOf(t));
         System.out.println("Available tickets:");
-        int i=1;
+        int i = 1;
         int tid;
-        if(tat)
-        {
+        if (tat) {
             for (Ticket x : t.tatkal) {
                 System.out.print(i++ + ". ");
                 System.out.println(x.type + " - " + x.count);
@@ -199,7 +192,7 @@ public class RailwayReservation {
             tid = t.tatkal.get(i - 1).tID;
         }
 
-        else{
+        else {
             for (Ticket x : t.available) {
                 System.out.print(i++ + ". ");
                 System.out.println(x.type + " - " + x.count);
@@ -210,16 +203,26 @@ public class RailwayReservation {
         }
         System.out.println("Enter number of Tickets");
         int count = Integer.parseInt(br.readLine());
-        Pnr p = u.bookTicket(t, count, tid,tat);
+        Pnr p = u.bookTicket(t, count, tid, tat, from, to);
         return p;
     }
 
     private static void viewPNR(Pnr p) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("PNR Status of " + p.pnrNo);
+        System.out.println("Ticket Type: " + p.t.type);
+        System.out.println("From: " + p.from + " To: " + p.to);
+        System.out.println("Bill: " + p.bill);
+        System.out.println("Number of tickets: " + p.count);
+        System.out.println("Booking Status: " + p.status);
     }
 
     private static void viewPNR(User u) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("Tickets booked by " + u.name);
+        int i = 1;
+        for (Pnr p : u.bookedPnr) {
+            System.out.print(i+++". ");
+            viewPNR(p);
+        }
     }
 
 }
